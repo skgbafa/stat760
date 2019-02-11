a1e23cdbe27c4c24f4809550654b05c7c7983e40
Sam Gbafa <skgbafa@gmail.com>
2019-02-10 18:25:19 -0800

    added panda dataframe for data storage

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # STAT 760 SPRING 2019 Homework
 
-
 ## Running Stuff
 This repo uses [pipenv](https://pipenv.readthedocs.io/en/latest/) for depenency and enviroment managment. Install instrunctions for it can be found there. Once installed `pipenv --help` can help orient you. To run the repo after installing `pipenv`
 
@@ -10,3 +9,13 @@ pipenv shell
 ```
 
 Then you can run any of the scripts in the `hw` folders. They correspond to each week of HW.
+
+## Repeated Commands
+This is stuff I keep doing and am leaving this here as a reference
+```
+pipenv install pandas
+```
+
+## Useful Links
+Links that I find useful for one reason or another
+- [Panda's Dataframe](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.html)
--- hw2/regression.py
+++ hw2/regression.py
@@ -4,6 +4,7 @@
 
 import csv
 import json
+import pandas as pd
 
 # import data
 def getData(trainFile):
@@ -11,11 +12,40 @@ def getData(trainFile):
     with open(trainFile) as csvfile:
         reader = csv.DictReader(csvfile,  delimiter='	')
         for row in reader:
+            # data cleanup
+            row.pop("", None)
+            for key in row.keys():
+                row[key] = float(row[key]) if (row[key] != 'T' and row[key] != 'F') else row[key]
             features.append(row)
-    return features
+        # convert to pandas dataframe 
+        keys = list(features[0].keys())
+        data = pd.DataFrame(features, columns=keys)
+    return data
+
+def getStats(data):
+    # calc stats
+    print(data.describe())
+    return {}
 
 if __name__ == "__main__":
-    trainFile = "./data/example.data"
+    # trainFile = "./data/example.data"
+    trainFile = "./data/prostate.data"
+
     # import data
     data = getData(trainFile)
-    print(json.dumps(data))
+    getStats(data)
+
+
+# {
+#     "": "1",
+#     "lcavol": "-0.579818495",
+#     "lweight": "2.769459",
+#     "age": "50",
+#     "lbph": "-1.38629436",
+#     "svi": "0",
+#     "lcp": "-1.38629436",
+#     "gleason": "6",
+#     "pgg45": "  0",
+#     "lpsa": "-0.4307829",
+#     "train": "T"
+# }
