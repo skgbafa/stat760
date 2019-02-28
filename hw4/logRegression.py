433e6cea7fee95c7fbdab22a4672a428959da004
Sam Gbafa <skgbafa@gmail.com>
2019-02-28 07:42:43 -0800

    added initial steps of homework

---
+++ hw4/logRegression.py
@@ -0,0 +1,103 @@
+# Samuel Gbafa
+# Homework 4
+# 02/28/19
+
+import csv
+import json
+import pandas as pd
+import numpy as np
+
+# import data
+def getData(trainFile):
+    with open(trainFile) as csvfile:
+        data = pd.read_csv(csvfile)
+        data['famhist'] = data['famhist'].map({'Present': 1, 'Absent': 0})
+        data.describe()
+    return data
+
+
+def initialBeta(numFeatures):
+
+    return np.matrix()
+
+
+def getX(data, numFeatures):
+    x = np.copy(data)
+    # replace first column with 1
+    x[::, 0] = 1
+    # keep n cols
+    return x[::, :numFeatures + 1]
+
+def probOfX(xi, beta):
+    return None
+
+def gradientLogBeta(x, y, beta):
+    # 
+    # return sum
+    return None
+
+def maximizeBeta(x, y, seedBeta, alpha, interations):
+    # initialize beta
+    currentBeta = seedBeta
+    newBeta = currentBeta
+    currentIteration = 0
+    
+    # while not max iterations or gradient diff is small
+    while currentIteration < interations:
+        # get current gradient
+        gradientStep = alpha * gradientLogBeta(x, y, currentBeta)
+        # update beta
+        newBeta = currentBeta + gradientStep
+        # increment
+        currentBeta = newBeta
+        currentIteration += 1
+        
+    # return initial beta
+    return newBeta
+
+def testModel(beta, x):
+    return None
+
+if __name__ == "__main__":
+    dataFile = "./data/example.data"
+    # dataFile = "./data/SAheart.data"
+
+    # import data
+    data = getData(dataFile)
+    
+    # convert data to matrix
+    matrixData = data.values
+    # get test and training data
+    testCount = int(matrixData.shape[0] * 0)
+    trainingData = matrixData[testCount:]
+    testingData = matrixData[:testCount]
+
+    # program params
+    numFeatures = 1
+
+    # math params
+    alpha = 1
+    
+    # initialize beta
+    seedBeta = initialBeta(numFeatures)
+
+    # get training data
+    x = getX(trainingData, numFeatures)
+
+    print(x[:2])
+    print(trainingData[:2])
+    
+    # # gradient decent
+    # maxBeta = maximizeBeta(x, seedBeta)
+
+    # # test model
+    # testX = getX(testingData, numFeatures)
+    # error = testModel(maxBeta, testX)
+
+    # # plot error
+    # # todo... in loop
+
+
+
+
+
